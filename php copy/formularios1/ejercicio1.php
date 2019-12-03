7d86d3797e612bbfa75aa5b758fa09429e042e71
usuario <marian.somesan.smr@gmail.com>
2019-12-03 09:50:05 +0100

    fgnfh

--- php copy/formularios1/ejercicio1.php
+++
@@ -1,8 +0,0 @@
-<?php
-$texto = $_POST["texto"];
-$tipo=$tipo = $_POST["tipo"];
-$genero=$_POST["genero"];
-print ("Texto buscado : $texto <br>");
-print ("Buscar en : $tipo <br>");
-print ("genero : $genero");
-?>
--- php copy/prueba.php
+++
@@ -1,7 +0,0 @@
-<?php
- function algo(){
-   // global $var==++;
-}
-$var=10;
-algo();
-?>
