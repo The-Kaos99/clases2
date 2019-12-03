e435c0af826549483862fcf4ded579e4770c825d
The-Kaos99 <marian.somesa.smr@gmail.com>
2019-12-03 12:20:55 +0100

    753

---
+++ entorno servidor/TODO/EjerciciosTema2/cabecera.php
@@ -0,0 +1,5 @@
+<?php
+    print '<html ><head><link href="eje1.css" rel="stylesheet" type="text/css">
+        </head>
+        <body class="'.$fondo.'">';
+?>
--- php/TODO/EjerciciosTema2/cabecera.php
+++
@@ -1,5 +0,0 @@
-<?php
-    print '<html ><head><link href="eje1.css" rel="stylesheet" type="text/css">
-        </head>
-        <body class="'.$fondo.'">';
-?>
