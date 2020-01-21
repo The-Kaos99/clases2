d7be49dcbc036f03910fb717193f33c8fc2792fb
The-Kaos99 <marian.somesan.smr@gmail.com>
2020-01-21 22:43:02 +0100

    eeee

--- entorno cliente/T5P1- Practica de validación de un formulario/validacion.js
+++ entorno cliente/T5P1- Practica de validación de un formulario/validacion.js
@@ -1,4 +1,33 @@
-valor = document.getElementById("nombre").value;
-if( valor == null || valor.length == 0 || /^\s+$/.test(valor) ) {
-  return false;
+
+function validar() {            
+  nombre = document.getElementById("nombre").value;
+  apellido=document.getElementById("apellido").value;
+  edad=document.getElementById("edad").value;
+  nif=document.getElementById("nif");
+  patronnif="[0-9]{8}[A-Z]";
+  if( nombre == null || nombre.length == 0 ){
+    alert("El nombre no puede ser vacío");  
+    document.getElementById("nombre").focus();
+    return false;
+  }
+  if( apellido == null || apellido.length == 0 ){
+    alert("El apellido no puede ser vacío");  
+    document.getElementById("apellido").focus();
+    return false;
+  }
+  if (isNaN(edad)) {
+    alert("La edad no puede ser letras");
+    document.getElementById("edad").focus();
+  }else{
+    if (edad>=0 && edad<=106) {
+     
+    }else{
+      alert("La edad no puede ser menor de 0 ni mayor de 105 años");
+      document.getElementById("edad").focus();
+    }
+  }
+  if (condition) {
+    
+  }
+  return true;
 }
