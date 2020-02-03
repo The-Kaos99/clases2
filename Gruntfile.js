0dd6436296f5d36e9489d331a5d6c9f08fe214a6
The-Kaos99 <marian.somesa.smr@gmail.com>
2020-02-03 10:19:14 +0100

    Acrena arregaldo

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,11 +23,8 @@ module.exports = function(grunt) {
 					engine: "gm",
 					sizes: [
 						{ name: "sm", suffix: "_1x", quality: 60, width: 600 },
-						{ name: "sm", suffix: "_2x", quality: 60, width: 1200 },
 						{ name: "md", suffix: "_1x", quality: 60, width: 900 },
-						{ name: "md", suffix: "_2x", quality: 60, width: 1800 },
-						{ name: "lg", suffix: "_1x", quality: 60, width: 1440 },
-						{ name: "lg", suffix: "_2x", quality: 60, width: 2880 }
+						{ name: "lg", suffix: "_1x", quality: 60, width: 1440 }
 					]        
 				},        
 				files: [{
