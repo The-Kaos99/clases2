ce63f920e4dc7736731bc0281125029f8de98517
The-Kaos99 <marian.somesa.smr@gmail.com>
2020-02-18 13:52:21 +0100

    1

--- entorno cliente/Somesan_Marian_Practica_Matricuals_Recuperacion/script.js
+++ entorno cliente/Somesan_Marian_Practica_Matricuals_Recuperacion/script.js
@@ -1,5 +1,7 @@
-
+var total = 0;
+var parejast = 0, triost = 0, pokert = 0;
 function calcular() {
+    var parejas = 0, trios = 0, poker = 0;
     var tipo = 0;
     var num = parseInt(document.getElementById("numero").value, 10);
     if (!isNaN(num) && num != "") {
@@ -7,11 +9,11 @@ function calcular() {
         var num_array = Array.from(auxiliar);
         if (num_array.length <= 4) {
             for (var index = 0; index < num_array.length; index++) {
-                var newt = document.createElement("div");
+                /*var newt = document.createElement("div");
                 newt.setAttribute("class", "bg-secondary border border-dark pt-5");
                 var t = document.createTextNode("El numero " + num_array[index]);
                 newt.appendChild(t);
-                // document.getElementById("prueba").appendChild(newt);
+                // document.getElementById("prueba").appendChild(newt);*/
                 var indices = [];
                 var element = num_array[index];
                 var idx = num_array.indexOf(element);
@@ -31,18 +33,25 @@ function calcular() {
             }
             switch (tipo) {
                 case 2:
-
+                    total++;
+                    parejas++;
+                    parejast++;
                     break;
                 case 3:
-
+                    total++;
+                    trios++;
+                    triost++;
                     break;
                 case 4:
-
+                    total++;
+                    poker++;
+                    pokert++;
                     break;
 
                 default:
                     break;
             }
+            vetana(total, parejas, trios, poker, parejast, triost, pokert);
 
         } else {
             alert('Has metido mas de 4 numeros');
@@ -50,7 +59,51 @@ function calcular() {
         }
 
     } else {
-        alert('Lo has dejado en blanco o has metidop lertras ');
+        alert('Lo has dejado en blanco o has metido lertras ');
         return false;
     }
+}
+
+function vetana(total, parejas, trios, poker, tparejas, ttrios, tpoker) {
+    var ventana = window.open("", "", "width=300 height=300");
+    var fondo = "#3EDD2E";
+    ventana.document.write("<h1>Hola es un titulo</h1>");
+    ventana.document.write("<p>Parejas : " + parejas + "</p>");
+    ventana.document.write("<p>Trios : " + trios + "</p>");
+    ventana.document.write("<p>Poker : " + poker + "</p>");
+    ventana.document.write("<p>Total figuras : " + total + "</p>");
+    ventana.document.write("<p>Parejas totales : " + tparejas + "</p>");
+    ventana.document.write("<p>Trios totales : " + ttrios + "</p>");
+    ventana.document.write("<p>Poker totales : " + tpoker + "</p>");
+    ventana.document.write("");
+    ventana.document.write(" <script src=\"./script.js\"></script>");
+    ventana.document.bgColor = fondo;
+    ventana.setTimeout(close, 10000);
+}
+var contador = 0;
+function calcularletras() {
+    var letras = ["B", "C", "D", "F", "G", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var letrasactuales = document.getElementById("letrasactuales").value;
+    var letrasActuales_array = Array.from(letrasactuales);
+    for (let index = 0; index < letrasActuales_array.length; index++) {
+        alert( letrasActuales_array[index]);
+        
+    }
+    for (let index = 0; index < letras.length; index++) {
+        for (let index2 = 0; index2 < letras.length; index2++) {
+            for (let index3 = 0; index3 < letras.length; index3++) {
+                contador++;
+                var newt = document.createElement("div");
+                newt.setAttribute("class", "col-12 text-info");
+                var t = document.createTextNode(letras[index] + letras[index2] + letras[index3]+" : "+contador);
+                newt.appendChild(t);
+                document.getElementById("prueba").appendChild(newt);
+
+            }
+
+        }
+    }
+}
+function igualdad(i,i2,i3,letras) {
+    
 }
